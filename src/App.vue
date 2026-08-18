7e9ec40d80364b7d1b4547bae168a1bd0dac6838
huanghu@eastmoney.com <huanghu@eastmoney.com>
2026-08-18 10:57:45 +0800

    优化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <a-layout style="min-height: 100vh">
+      <a-layout-content style="padding: 24px">
+        <router-view />
+      </a-layout-content>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+    'Noto Sans', sans-serif;
+}
+</style>
+
